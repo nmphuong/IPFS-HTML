d6165cd91ad56895eb3204334112a968fdab71e6
nmphuong <nguyenminhphuong25111999@gmail.com>
2021-09-01 12:53:07 +0700

    init code

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+    var result = null
+    $('#btn-submit').addClass('btn-disabled')
+    $("#file").on('change', function() {
+        event.preventDefault()
+        const file = event.target.files[0]
+        const reader = new window.FileReader()
+        reader.readAsArrayBuffer(file)
+        reader.onloadend = (e) => {
+            result = reader.result
+        }
+        $('#btn-submit').removeClass('btn-disabled')
+    })
+    $('#btn-submit').on("click", async function() {
+        event.preventDefault()
+        if (result == null) {
+            alert('Choose file please!')
+        } else {
+            var ipfs = await window.IpfsHttpClient.create({
+                host: 'ipfs.infura.io',
+                port: 5001,
+                protocol: 'https'
+            })
+            var data = await ipfs.add(result)
+            $('#image-show').attr('src', `https://cloudflare-ipfs.com/ipfs/${data.path}`)
+            var string = `
+                <p><a href="ipfs://${data.path}">ipfs://${data.path}</a></p>
+                <p><a href="https://ipfs.infura.io/ipfs/${data.path}">https://ipfs.infura.io/ipfs/${data.path}</a></p>
+                <p><a href="https://cloudflare-ipfs.com/ipfs/${data.path}">https://cloudflare-ipfs.com/ipfs/${data.path}</a></p>
+            `
+            $('#url').append(string)
+        }
+    })
+})
